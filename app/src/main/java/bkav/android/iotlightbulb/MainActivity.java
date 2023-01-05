911f7db2a421ba57ada829cc637ca982252f3e14
dovietphuc <dovietphuc0131@gmail.com>
2023-01-05 23:40:18 +0700

    Bkav PhucDVb:

--- app/src/main/java/bkav/android/iotlightbulb/MainActivity.java
+++ app/src/main/java/bkav/android/iotlightbulb/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
@@ -14,42 +15,74 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import bkav.android.iotlightbulb.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    final String CLIENT_ATTR_TOPIC = "v1/devices/me/attributes";
+    final String DEVICE_ACCESS_TOKEN = "wgUwf4sxGxHFNnTpqusu";
+
+    ActivityMainBinding mBinding;
+    String mClientId = MqttClient.generateClientId();
+    MqttAndroidClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
-        String serverUri = "tcp://demo.thingsboard.io:1883";
-        String clientId = MqttClient.generateClientId();
-        String accessToken = "wgUwf4sxGxHFNnTpqusu";
-        MqttAndroidClient client = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
-        client.setCallback(new MqttCallbackExtended() {
+        mBinding.switch1.setOnCheckedChangeListener((compoundButton, b) -> {
+            if(mClient != null && mClient.isConnected()){
+                publish(CLIENT_ATTR_TOPIC, ("{\"isTurningOn\":" + b + "}").getBytes());
+            }
+        });
+
+        connectThingsboard("tcp://demo.thingsboard.io:1883", DEVICE_ACCESS_TOKEN);
+    }
+
+    public void publish(String topic, byte[] message){
+        try {
+            mClient.publish(topic, message, 0, false);
+        } catch (MqttException e) {
+            Log.d("PhucDVb", "publish: ", e);
+            e.printStackTrace();
+        }
+    }
+
+    public void subscribe(String topic){
+        try {
+            mClient.subscribe(topic, 0);
+        } catch (MqttException e) {
+            Log.d("PhucDVb", "subscribe: ", e);
+            e.printStackTrace();
+        }
+    }
+
+    public void connectThingsboard(String serverUrl, String accessToken){
+        mClient = new MqttAndroidClient(getApplicationContext(), serverUrl, mClientId);
+        mClient.setCallback(new MqttCallbackExtended() {
             @Override
             public void connectComplete(boolean reconnect, String serverURI) {
                 Log.d("PhucDVb", "connectComplete: " + reconnect);
-                // Bkav PhucDVb: v1/devices/me/attributes
-                try {
-                    client.publish("v1/devices/me/attributes", "{\"isTurningOn\": false}".getBytes(), 0, false);
-                } catch (MqttException e) {
-                    Log.d("PhucDVb", "connectComplete: ", e);
-                    e.printStackTrace();
-                }
+                Toast.makeText(MainActivity.this, reconnect ? "Reconnected" : "Connected", Toast.LENGTH_SHORT).show();
+                publish(CLIENT_ATTR_TOPIC,  ("{\"isTurningOn\":" + mBinding.switch1.isChecked() + "}").getBytes());
+
+                subscribe("v1/devices/me/rpc/request/+");
             }
 
             @Override
             public void connectionLost(Throwable cause) {
                 Log.d("PhucDVb", "connectionLost: ", cause);
+                Toast.makeText(MainActivity.this, "Lost connection. Trying to reconnect", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void messageArrived(String topic, MqttMessage message) throws Exception {
                 Log.d("PhucDVb", "messageArrived: " + message.toString());
+                mBinding.switch1.setChecked(isTuningOn(new String(message.getPayload())));
             }
 
             @Override
@@ -64,25 +97,29 @@ public class MainActivity extends AppCompatActivity {
         mqttConnectOptions.setUserName(accessToken);
 
         try {
-            client.connect(mqttConnectOptions, null, new IMqttActionListener() {
+            mClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    Log.d("PhucDVb", "onSuccess: ");
+                    Log.d("PhucDVb", "onSuccess: " + asyncActionToken);
                 }
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    Log.d("PhucDVb", "onFailure: ");
+                    Log.d("PhucDVb", "onFailure: ", exception);
                 }
             });
-
-
         } catch (MqttException ex){
             ex.printStackTrace();
         }
+    }
 
-        binding.switch1.setOnCheckedChangeListener((compoundButton, b) -> {
-
-        });
+    public boolean isTuningOn(String json){
+       try {
+           String method = new JSONObject(json).getString("method");
+           return "turnOn".equals(method);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+       return false;
     }
 }
